09dae7a96dd171dbed693e8cdcd5221d018a6c34
tuongchau20 <chauquangtuong.a3.2018@gmail.com>
2023-12-13 14:08:32 +0700

    Add project files.

---
+++ WebApi/Controllers/ValuesController.cs
@@ -0,0 +1,79 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public ValuesController(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+        }
+
+        [HttpGet("GetAll")]
+        public async Task<IActionResult> GetAll()
+        {
+            try
+            {
+                var apiUrl = "https://restcountries.com/v3.1/all";
+                var httpClient = _httpClientFactory.CreateClient();
+
+                var jsonResponse = await httpClient.GetStringAsync(apiUrl);
+
+                return Ok(jsonResponse);
+            }
+            catch (HttpRequestException)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, "Error");
+            }
+        }
+
+        [HttpGet("GetByCCA2/{cca2}")]
+        public async Task<IActionResult> GetByCCA2(string cca2)
+        {
+            try
+            {
+                var httpClient = _httpClientFactory.CreateClient();
+                var response = await httpClient.GetAsync($"https://restcountries.com/v3.1/alpha/{cca2}");
+
+                if (response.IsSuccessStatusCode)
+                {
+                    return Ok(await response.Content.ReadAsStringAsync());
+                }
+
+                return NotFound();
+            }
+            catch (HttpRequestException)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, "Error");
+            }
+        }
+
+        [HttpGet("GetByAlphaCode/{alphaCode}")]
+        public async Task<IActionResult> GetByAlphaCode(string alphaCode)
+        {
+            try
+            {
+                var httpClient = _httpClientFactory.CreateClient();
+                var response = await httpClient.GetAsync($"https://restcountries.com/v3.1/alpha/{alphaCode}");
+
+                if (response.IsSuccessStatusCode)
+                {
+                    return Ok(await response.Content.ReadAsStringAsync());
+                }
+
+                return NotFound();
+            }
+            catch (HttpRequestException)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, "Error");
+            }
+        }
+    }
+}
---
+++ WebApi/Program.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.Extensions.Configuration;
+using Nest;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddControllers();
+builder.Services.AddHttpClient();
+
+// Register Elasticsearch
+var elasticsearchUri = builder.Configuration.GetConnectionString("ElasticsearchConnection");
+
+// Cấu hình Elasticsearch
+var settings = new ConnectionSettings(new Uri(elasticsearchUri))
+    .DefaultIndex("your_default_index"); 
+
+var client = new ElasticClient(settings);
+
+builder.Services.AddSingleton<IElasticClient>(client);
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+app.MapControllers();
+
+app.Run();
